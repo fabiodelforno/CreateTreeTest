8a073ddeb3e8e3dea237f05ecc920bf0c4310a36
fabiodelforno <unifabio86@gmail.com>
2016-04-06 19:43:53 +0200

    calcolo nodi

--- CreateTreeTest/CreateTreeTest.Designer.cs
+++ CreateTreeTest/CreateTreeTest.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.depthBox = new System.Windows.Forms.TextBox();
+            this.splitBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numeri di nodi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Depth";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Split-Size";
+            // 
+            // depthBox
+            // 
+            this.depthBox.Location = new System.Drawing.Point(17, 50);
+            this.depthBox.Name = "depthBox";
+            this.depthBox.Size = new System.Drawing.Size(38, 20);
+            this.depthBox.TabIndex = 4;
+            // 
+            // splitBox
+            // 
+            this.splitBox.Location = new System.Drawing.Point(88, 50);
+            this.splitBox.Name = "splitBox";
+            this.splitBox.Size = new System.Drawing.Size(45, 20);
+            this.splitBox.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 20);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(15, 98);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Numero di attributi totali";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(160, 99);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 118);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(157, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Lista Nomi attributi";
+            // 
+            // CreateTreeTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.splitBox);
+            this.Controls.Add(this.depthBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateTreeTest";
             this.Text = "CreateTreeTest";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox depthBox;
+        private System.Windows.Forms.TextBox splitBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- CreateTreeTest/CreateTreeTest.cs
+++ CreateTreeTest/CreateTreeTest.cs
@@ -15,6 +15,41 @@ namespace CreateTreeTest
         public CreateTreeTest()
         {
             InitializeComponent();
+            
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            calculateNumberNode();
+        }
+
+        private void calculateNumberNode()
+        {
+            int nodi = 0;
+            int depth = int.Parse(depthBox.Text);
+            int split_size = int.Parse(splitBox.Text);
+            if (depth == 1) return;
+            if (split_size != 2 && depth != 2)
+            {
+                nodi = (int)Math.Pow(split_size, depth - 1);
+                nodi += split_size;
+                Console.WriteLine("numero nodi senza root: " + nodi);
+            }else
+            {
+                if(split_size == 2)
+                {
+                    nodi = (int)Math.Pow(split_size, depth) - 1;
+                    Console.WriteLine("numero nodi senza root split=2: " + nodi);
+                }
+                else
+                {
+                    nodi += split_size;
+                    Console.WriteLine("numero nodi senza root depth=2: " + nodi);
+                }
+            }
+
+        }
+        
     }
 }
